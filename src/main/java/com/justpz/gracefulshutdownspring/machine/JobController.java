866391f3d03a75d443f219fe7005c82ac800e30f
Przemek Zawadzki <pzawadzki95@gmail.com>
2019-12-23 15:09:17 +0100

    add logger to endpoints

--- src/main/java/com/justpz/gracefulshutdownspring/machine/JobController.java
+++ src/main/java/com/justpz/gracefulshutdownspring/machine/JobController.java
@@ -1,21 +1,32 @@
 package com.justpz.gracefulshutdownspring.machine;
 
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JobController {
 
+  private static Logger logger = LoggerFactory.getLogger(JobController.class);
+
   @GetMapping("small")
   public String runSmallJob() throws InterruptedException {
+    String uuid = UUID.randomUUID().toString();
+    logger.debug("start small job {}", uuid);
     Thread.sleep(10_000);
-    return String.format("small id [%d]", Thread.currentThread().getId());
+    logger.debug("end small job {}", uuid);
+    return String.format("small id [%d] uuid [%s]", Thread.currentThread().getId(), uuid);
   }
 
   @GetMapping("huge")
   public String runHugeJob() throws InterruptedException {
+    String uuid = UUID.randomUUID().toString();
+    logger.debug("start huge job {}", uuid);
     Thread.sleep(50_000);
-    return String.format("huge id [%d]", Thread.currentThread().getId());
+    logger.debug("end huge job {}", uuid);
+    return String.format("huge id [%d] uuid [%s]", Thread.currentThread().getId(), uuid);
   }
 
 }
